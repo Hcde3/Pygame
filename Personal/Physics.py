684301bc32b843728086c619671ff14c3f44743b
Harry Daniel <145117624+Hcde3@users.noreply.github.com>
2023-11-17 12:17:51 +0000

    Update Physics.py

--- Personal/Physics.py
+++ Personal/Physics.py
@@ -1,20 +1,35 @@
 from sys import exit
 import pygame
 
+def Archimedes(Density,Depth,Gravity,Surf,Height):
+    Pressure = Density*Depth*Gravity
+    F = Surf*Pressure
+    Pressure = Density*(Depth+Height)*Gravity
+    F -= Surf*Pressure
+    return F
+
+Wh = int(input("Water Height(m):  "))
+Rs = int(input("Rectangle Size(cm):  "))
+Wh = 895-(Wh*100)
 pygame.init()
-screen = pygame.display.set_mode((800,500))
+screen = pygame.display.set_mode((800,1000))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Move Rect")
-background = pygame.Surface((800,500))
-rectangle = pygame.Surface((60,60))
-rectangle.fill("Purple")
-rect = rectangle.get_rect(center = (400,250))
+background = pygame.Surface((800,1000))
+rectangle = pygame.Surface((Rs,Rs))
+rectangle.fill("White")
+rect = rectangle.get_rect(center = (400,500))
 floor = pygame.Surface((800,1000))
 floor.fill("Dark Green")
-floor_r = floor.get_rect(topleft = (0,395))
+floor_r = floor.get_rect(topleft = (0,895))
+
+water = pygame.Surface((800,800))
+water.fill("Blue")
+water_r = water.get_rect(topleft = (0,Wh))
 
 Velocity = 0
-Mass = 1
+Mass = ((Rs/100)**3)*4.6
+print(Mass)
 Acceleration = []
 Force = 0
 setgrav = 0.5
@@ -34,12 +49,15 @@ while True:
     Acceleration.append(grav)
     for x in range(len(Acceleration)):
         Force += Mass*Acceleration[x]
-    if rect.y > 335:
+    if rect.y > floor_r.y-Rs:
         if Velocity > 0:
             Force = Force*-1 - (Velocity*1.3)
-        rect.y = 335
-    else:
-        grav = setgrav
+        rect.y = floor_r.y-Rs
+    if Wh != 895:
+        floor.fill("Yellow")
+    if rect.y > Wh-Rs:
+        Force += Archimedes(10,(rect.y-Wh-Rs)/100,setgrav,(Rs/100)**2,Rs/100)
+        Velocity *= 0.99 # Water Resistance not finished works best at 60cm
     Velocity += Force
     rect.y += Velocity
     Acceleration = []
@@ -48,7 +66,8 @@ while True:
     
     ##Blitting##
     screen.blit(background,(0,0))
+    screen.blit(water,water_r)
     screen.blit(floor,floor_r)
     screen.blit(rectangle,rect)
     pygame.display.update()
-    clock.tick(90)
+    clock.tick(120)
